942d7a05df48fb420f5b6dba8eedac7edf0d9e83
Seth Woodworth <seth@sethish.com>
2015-01-28 12:22:59 -0500

    add setup for venv python3 style

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 # clone shakespeare texts
 git clone --depth=1 git@github.com:okfn/shakespeare-material.git ./texts
+
+# use python3 tools to setup/activate virtualenv
+pyvenv-3.4 venv
+source venv/bin/activate
+
+
